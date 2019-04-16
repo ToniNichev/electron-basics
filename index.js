88d503b556b100730c5a914d4082be6d44e84cc7
Toni Nichev <toni.nichev@nbcuni.com>
2019-04-16 17:37:40 -0400

    Work

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const {app, ipcMain, BrowserWindow} = require('electron');
-
+let win = null;
   
 app.on('ready', function() {
 
 
   // Create the browser window.
-  let win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 800, height: 600 })
 
   // and load the index.html of the app.
   win.loadFile('windows/one/one.html');
   win.webContents.openDevTools();
 
 });
+
+ipcMain.on('send-message-to-renderer', (event, newConfig) => {
+  console.log(">>>> send-message-to-renderer", newConfig);
+  win.webContents.send('send-message-to-window', {"message": "message from renderer!"});
+});
+
+
 
 
 
--- windows/one/one.js
+++ windows/one/one.js
@@ -1,4 +1,12 @@
+let ipcRenderer = require('electron').ipcRenderer;
+
 document.getElementById("buttonOne").addEventListener("click", function (e) {
-  //window.ipcRenderer.send('request-showing-of-settting-window');
+  ipcRenderer.send('send-message-to-renderer', {"one": 1});
   console.log("!@!@");
+});
+
+
+ipcRenderer.on('send-message-to-window', (event, payload) => {
+  console.log("end-message-to-window");
+  console.log(payload);
 });
