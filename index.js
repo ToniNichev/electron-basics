45bff91766515166af548ce4e1afe6e62b05fffd
ToniNichev <toni.nichev@gmail.com>
2019-04-16 20:02:42 -0400

    work

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const {app, ipcMain, BrowserWindow} = require('electron');
+const fs = require('fs');
+const path = require('path');
+
 let win = null;
+let filePath = 'app-settings.json';
   
 app.on('ready', function() {
 
@@ -11,11 +15,54 @@ app.on('ready', function() {
   win.loadFile('windows/one/one.html');
   win.webContents.openDevTools();
 
+  setTimeout(() => {
+    loadSettings();
+  }, 2000);
+
+
 });
 
-ipcMain.on('send-message-to-renderer', (event, newConfig) => {
-  console.log(">>>> send-message-to-renderer", newConfig);
-  win.webContents.send('send-message-to-window', {"message": "message from renderer!"});
+
+function loadSettings() {
+  filepath = path.join(filePath);
+
+  if (fs.existsSync(filepath)) {
+    //settings exists
+    fs.readFile(filepath, 'utf-8', (err, data) => {
+      if(err){
+          console.log("Error loading settings!");
+          return;
+      }    
+      var data = JSON.parse(data); 
+      console.log(data);
+      win.webContents.send('send-message-to-window-save', data);
+    });        
+  }
+  else {
+    console.log("No config file, let's create it for the first time");
+    // create config for the first time
+    //this.config  = JSON.parse('{"firstTimeRun":1,"syncConfigs":[]}');
+    //this.saveSettings(this.config, () => { this.loadSettings(); });      
+  }
+}
+
+
+ipcMain.on('send-message-to-renderer', (event, data) => {
+  console.log(">>>> send-message-to-renderer", data);
+  filepath = path.join(filePath);   
+  data = JSON.stringify(data);
+  fs.writeFile(filepath, data,  function (err) {
+    console.log("SAVED");
+    win.webContents.send('send-message-to-window', {"message": "Saved successfully!"});
+    if(err){
+        console.log("An error ocurred reading the file :" + err.message);
+        win.webContents.send('send-message-to-window', {"message": "Error saving data!", "error": err.message});
+        return; 
+    }else {
+    }    
+  });  
+
+  //win.webContents.send('send-message-to-window', {"message": "message from renderer!"});
 });
 
 
--- windows/one/one.js
+++ windows/one/one.js
@@ -1,12 +1,17 @@
 let ipcRenderer = require('electron').ipcRenderer;
 
 document.getElementById("buttonOne").addEventListener("click", function (e) {
-  ipcRenderer.send('send-message-to-renderer', {"one": 1});
-  console.log("!@!@");
+  var data = document.getElementById("textFieldOne").value;
+  ipcRenderer.send('send-message-to-renderer', {"data": data});
 });
 
 
 ipcRenderer.on('send-message-to-window', (event, payload) => {
   console.log("end-message-to-window");
   console.log(payload);
+});
+
+
+ipcRenderer.on('send-message-to-window-save', (event, payload) => {
+  document.getElementById("textFieldOne").value = payload.data;
 });
